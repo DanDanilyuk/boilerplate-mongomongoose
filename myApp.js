3154d46526fea6553b2eeb777f45f1e923d66f44
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-09 15:36:54 -0600

    Fix bug in MongoDB URL

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const mySecret = process.env['MONGO_URI'];
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
