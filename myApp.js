7eac6eb3f3641d6b0494aadb0309d31686ca0788
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-10 19:00:12 -0600

    Create find and update method

--- myApp.js
+++ myApp.js
@@ -61,7 +61,7 @@ const findEditThenSave = (personId, done) => {
   Person.findById({ _id: personId }, function(err, personFound) {
     if (err) return console.log(err);
     personFound.favoriteFoods.push(foodToAdd);
-    personFound .save(function(err, data) {
+    personFound.save(function(err, data) {
       if (err) return console.error(err);
       done(null, data);
     })
@@ -71,7 +71,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+    if(err) return console.log(err);
+    done(null, updatedDoc);
+  })
 };
 
 const removeById = (personId, done) => {
