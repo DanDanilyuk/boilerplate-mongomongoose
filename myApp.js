6e6fd4a6db2e2e7ae6068991df3b56a7aefee2b4
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-10 18:49:43 -0600

    Add find edit and save endpoint

--- myApp.js
+++ myApp.js
@@ -58,7 +58,14 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, function(err, personFound) {
+    if (err) return console.log(err);
+    personFound.favoriteFoods.push(foodToAdd);
+    personFound .save(function(err, data) {
+      if (err) return console.error(err);
+      done(null, data);
+    })
+  });
 };
 
 const findAndUpdate = (personName, done) => {
