0df9c8616408b15911c76f3da67e9d766d3720cf
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-10 19:15:19 -0600

    Add removal of many people

--- myApp.js
+++ myApp.js
@@ -87,7 +87,10 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove}, (err, peopleToRemove) => {
+    if(err) return console.log(err);
+    done(null, peopleToRemove);
+  });
 };
 
 const queryChain = (done) => {
